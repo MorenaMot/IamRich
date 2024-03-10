c014d5a22f14e7f1e21f414c3c60119fbe82dde7
morenamot <motaung.thabiso@gmail.com>
2024-03-10 13:46:40 +0200

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Thabiso Motaung on 2024/03/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
